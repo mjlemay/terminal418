366c920f998193b6aeef875efeb7936958870de5
Michael LeMay <mjlemay@gmail.com>
2024-12-01 15:06:12 -0800

    initial commit of next.js in tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,13 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// fn main() {
+//     terminal418_lib::run()
+// }
+
+
+fn main() {
+tauri::Builder::default()
+   .run(tauri::generate_context!())
+   .expect("error while running tauri application");
+}
